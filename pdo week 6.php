4284739bc4f1e5f6c1744c8e96afc1b8e829ee84
2167706 <133228756+2167706@users.noreply.github.com>
2023-06-15 21:05:22 +0200

    Add files via upload

---
+++ pdo week 6.php
@@ -0,0 +1,22 @@
+<?php
+$tijd = date("H:i");
+
+if ($tijd < "12:00") {
+echo "Goedemorgen";
+} elseif ($tijd >= "12:00" && $tijd < "18:00") {
+echo "Goedemiddag";
+} else {
+echo "Goedenavond";
+}
+?>
+
+<?php
+function bereken($getal1, $getal2) {
+$gemiddelde = ($getal1 + $getal2) / 2;
+echo "gemiddelde = " . $gemiddelde;
+}
+
+$getal1 = 16;
+$getal2 = 2;
+bereken($getal1, $getal2);
+?>
